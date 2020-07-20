cdc1957c514579ef7be6b4cfd9850a334695a6b0
Xavier Santiago <xavier@santiagoville.net>
2020-07-20 13:59:13 -0700

    added foundation, no movement yet

--- src/PixelSandbox.java
+++ src/PixelSandbox.java
@@ -4,15 +4,28 @@ import java.awt.*;
 public class PixelSandbox {
     JFrame frame = new JFrame();
     Renderer renderer = new Renderer();
+    public static Pixel[][] grid;
+    static int w = 500;
+    static int h = 500;
+    static int pixelSize = 10;
 
     public static void main(String[] args) {
+
+        grid = new Pixel[w/pixelSize][h/pixelSize];
+        for (int x = 0; x < w/pixelSize; x++){
+          for (int y = 0; y < h/pixelSize; y++){
+            grid[x][y] = new Pixel("air", x, y);
+          }
+        }
+        grid[25][25] = new Sand(25, 25);
+
         new PixelSandbox();
     }
 
     private PixelSandbox() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout());
-        frame.setSize(250, 250);
+        frame.setSize(w, h);
 
         frame.add(renderer);
 
--- src/Renderer.java
+++ src/Renderer.java
@@ -4,7 +4,14 @@ import java.awt.*;
 public class Renderer extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.black);
-        g.drawString("Hello, World!", 90, 100);
+        //g.drawString("Hello, World!", 90, 100);
+        for (int x = 0; x < PixelSandbox.grid.length; x++){
+            for (int y = 0; y < PixelSandbox.grid.length; y++){
+                if (PixelSandbox.grid[x][y].type == "sand"){
+                    g.setColor(Color.black);
+                    g.drawRect(x, y, PixelSandbox.pixelSize, PixelSandbox.pixelSize);
+                }
+            }
+        }
     }
 }
