abb47c60e25a09868f978d742450e93826ac144b
Niko E <exoticbirdsoftheworld@gmail.com>
2020-07-20 12:12:51 -0700

    checkerboard test

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,12 +1,27 @@
-package src;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class Renderer extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.black);
-        g.drawString("Hello, World!", 90, 100);
+        int width = 20;
+        int height = 20;
+
+        int pixelsPerSquare = Math.min(getWidth() / width, getHeight() / height);
+
+        boolean coloredBlack = true;
+
+        for (double y = 0; y < height * pixelsPerSquare; y += pixelsPerSquare) {
+            for (double x = 0; x < width * pixelsPerSquare; x += pixelsPerSquare) {
+                if(coloredBlack) {
+                    g.setColor(Color.black);
+                } else {
+                    g.setColor(Color.white);
+                }
+                coloredBlack = !coloredBlack;
+                g.fillRect((int) x, (int) y, pixelsPerSquare, pixelsPerSquare);
+            }
+            coloredBlack = !coloredBlack;
+        }
     }
 }
