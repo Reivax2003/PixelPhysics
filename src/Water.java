6214ae5588680df54ff0c965fa52b9b56be80754
jupiter peng <pengjupiter5@gmail.com>
2020-07-21 12:36:44 -0700

    added out of bounds check and changed direction for water pixel

--- src/Water.java
+++ src/Water.java
@@ -9,15 +9,15 @@ public class Water extends Pixel {
     public int[] update(Grid grid) {
         int[] newPosition = new int[] {x, y};
 
-        if (grid.getPixel(x, y - 1).getType().equals("air")) {
+        if (y < grid.getHeight()-1 && grid.getPixel(x, y + 1).getType().equals("air")) {
             newPosition[0] = x;
-            newPosition[1] = y - 1;
+            newPosition[1] = y + 1;
         }
-        else if (grid.getPixel(x-1, y).getType().equals("air") && Math.random() > 0.5) {
+        else if (x > 0 && grid.getPixel(x-1, y).getType().equals("air") && Math.random() > 0.5) {
             newPosition[0] = x - 1;
             newPosition[1] = y;
         }
-        else if (grid.getPixel(x+1, y).getType().equals("air")) {
+        else if (x < grid.getWidth()-1 && grid.getPixel(x+1, y).getType().equals("air")) {
             newPosition[0] = x + 1;
             newPosition[1] = y;
         }
