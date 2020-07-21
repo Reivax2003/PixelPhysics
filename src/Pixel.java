d0c3b0dcd2c89687fe155d5971e05b78b710c133
Robert Young <ryoungster@comcast.net>
2020-07-21 10:29:29 -0700

    Update function made

--- src/Pixel.java
+++ src/Pixel.java
@@ -29,4 +29,9 @@ public class Pixel {
   public void setType(String type) {
     this.type = type;
   }
+
+  public int[] update(Grid grid) {  
+      int[] newPosition = new int[] {x, y};
+      return newPosition;
+  }
 }
