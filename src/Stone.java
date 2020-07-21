3d3bbc520f4e53b1a7a634db49eb34cc77a761b1
jupiter peng <pengjupiter5@gmail.com>
2020-07-21 10:48:03 -0700

    added stone pixel

---
+++ src/Stone.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class Stone extends Pixel {
+
+    public Stone(int xpos, int ypos) {
+        super("stone", xpos, ypos, Color.gray);
+    }
+    
+    public int[] update(Grid grid) {
+        return new int[] { x, y };
+    }
+}
