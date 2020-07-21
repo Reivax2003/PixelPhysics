4902ecd8bb7dea471f39b77f181736a595dcce36
Robert Young <ryoungster@comcast.net>
2020-07-21 14:33:17 -0700

    Made air exception empty

--- src/sandbox/pixels/Sand.java
+++ src/sandbox/pixels/Sand.java
@@ -13,15 +13,15 @@ public class Sand extends Pixel implements Granular {
     public int[] update(Grid grid) {
         int[] newPosition = new int[] {x, y};
 
-        if ((y < grid.getHeight()-1 && (grid.getPixel(x, y + 1).getType().equals("air") ||  grid.getPixel(x, y + 1) instanceof Liquid ||  grid.getPixel(x, y + 1) instanceof Gas)) {
+        if ((y < grid.getHeight()-1 && (grid.getPixel(x, y + 1) instanceof Empty ||  grid.getPixel(x, y + 1) instanceof Liquid ||  grid.getPixel(x, y + 1) instanceof Gas)) {
             newPosition[0] = x;
             newPosition[1] = y + 1;
         }
-        else if (x > 0 && y < grid.getHeight()-1 && (grid.getPixel(x, y + 1).getType().equals("air") || grid.getPixel(x-1, y + 1) instanceof Liquid || grid.getPixel(x-1, y + 1) instanceof Gas) {
+        else if (x > 0 && y < grid.getHeight()-1 && (grid.getPixel(x-1, y + 1) instanceof Empty || grid.getPixel(x-1, y + 1) instanceof Liquid || grid.getPixel(x-1, y + 1) instanceof Gas) {
             newPosition[0] = x - 1;
             newPosition[1] = y + 1;
         }
-        else if (x < grid.getWidth()-1 && y < grid.getHeight()-1 && (grid.getPixel(x, y + 1).getType().equals("air") || grid.getPixel(x+1, y + 1) instanceof Liquid || grid.getPixel(x+1, y + 1) instanceof Gas) {
+        else if (x < grid.getWidth()-1 && y < grid.getHeight()-1 && (grid.getPixel(x+1, y + 1) instanceof Empty || grid.getPixel(x+1, y + 1) instanceof Liquid || grid.getPixel(x+1, y + 1) instanceof Gas) {
             newPosition[0] = x + 1;
             newPosition[1] = y + 1;
         }
