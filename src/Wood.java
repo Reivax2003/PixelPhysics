393773398cbc00ec713b2187274a1957153523f5
Xavier Santiago <xavier@santiagoville.net>
2020-07-21 11:28:38 -0700

    Fixed fire errors

--- src/Wood.java
+++ src/Wood.java
@@ -2,7 +2,10 @@ import java.awt.*;
 
 public class Wood extends Pixel {
 
-    public Sand(int xpos, int ypos) {
-        super("wood", xpos, ypos, Color.brown);
+    public Wood(int xpos, int ypos) {
+        super("wood", xpos, ypos, Color.orange);
+    }
+    public int[] update(Grid grid) {
+        return new int[] { x, y };
     }
 }
