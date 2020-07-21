8391c526aaee29435094afec756a61e5455df759
Xavier Santiago <xavier@santiagoville.net>
2020-07-21 13:22:05 -0700

    Update Granular.java

--- src/Granular.java
+++ src/Granular.java
@@ -5,15 +5,15 @@ public class Granular {
     public int[] update(Grid grid) {
         int[] newPosition = new int[] {x, y};
 
-        if (y < grid.getHeight()-1 && grid.getPixel(x, y + 1).getType().equals("air")) {
+        if (y < grid.getHeight()-1 && grid.getPixel(x, y + 1) instanceof Liquid || y < grid.getHeight()-1 && grid.getPixel(x, y + 1) instanceof Gas) {
             newPosition[0] = x;
             newPosition[1] = y + 1;
         }
-        else if (x > 0 && y < grid.getHeight()-1 && grid.getPixel(x-1, y + 1).getType().equals("air")) {
+        else if (x > 0 && y < grid.getHeight()-1 && grid.getPixel(x-1, y + 1) instanceof Liquid || x > 0 && y < grid.getHeight()-1 && grid.getPixel(x-1, y + 1) instanceof Gas) {
             newPosition[0] = x - 1;
             newPosition[1] = y + 1;
         }
-        else if (x < grid.getWidth()-1 && y < grid.getHeight()-1 && grid.getPixel(x+1, y + 1).getType().equals("air")) {
+        else if (x < grid.getWidth()-1 && y < grid.getHeight()-1 && grid.getPixel(x+1, y + 1) instanceof Liquid || x < grid.getWidth()-1 && y < grid.getHeight()-1 && grid.getPixel(x+1, y + 1) instanceof Gas) {
             newPosition[0] = x + 1;
             newPosition[1] = y + 1;
         }
