42271d88f435b3bd061932d3d34cd5413029ee23
jupiter peng <pengjupiter5@gmail.com>
2020-07-21 12:44:31 -0700

    added oob check and changed direction for sand pixel

--- src/Sand.java
+++ src/Sand.java
@@ -9,17 +9,17 @@ public class Sand extends Pixel {
     public int[] update(Grid grid) {
         int[] newPosition = new int[] {x, y};
 
-        if (grid.getPixel(x, y + 1).getType().equals("air")) {
+        if (y < grid.getHeight()-1 && grid.getPixel(x, y + 1).getType().equals("air")) {
             newPosition[0] = x;
             newPosition[1] = y + 1;
         }
-        else if (grid.getPixel(x-1, y - 1).getType().equals("air")) {
+        else if (x > 0 && y < grid.getHeight()-1 && grid.getPixel(x-1, y + 1).getType().equals("air")) {
             newPosition[0] = x - 1;
-            newPosition[1] = y - 1;
+            newPosition[1] = y + 1;
         }
-        else if (grid.getPixel(x+1, y - 1).getType().equals("air")) {
+        else if (x < grid.getWidth()-1 && y < grid.getHeight()-1 && grid.getPixel(x+1, y + 1).getType().equals("air")) {
             newPosition[0] = x + 1;
-            newPosition[1] = y - 1;
+            newPosition[1] = y + 1;
         }
         return newPosition;
     }
