8e2801e45921fec10240292ebe757bb8d1afa338
Xavier Santiago <xavier@santiagoville.net>
2020-07-22 14:54:47 -0700

    F I R E

--- src/sandbox/GameLogic.java
+++ src/sandbox/GameLogic.java
@@ -1,9 +1,11 @@
 package sandbox;
 
 import sandbox.pixels.Pixel;
+import sandbox.pixels.Air;
 
 import javax.swing.*;
 import java.util.TimerTask;
+import java.util.ArrayList;
 
 public class GameLogic extends TimerTask {
 
@@ -25,6 +27,7 @@ public class GameLogic extends TimerTask {
         for (int y = grid.getHeight() - 1; y > -1; y--) {
             for (int x = (reverse? 1:0) * (grid.getWidth() -1); -1 < x && x < grid.getWidth(); x+= reverse? -1:1 ) {
                 Pixel currentPixel = grid.getPixel(x, y);
+
                 int currentX = currentPixel.getX();
                 int currentY = currentPixel.getY();
                 currentPixel.setMoved(false);
@@ -74,10 +77,147 @@ public class GameLogic extends TimerTask {
                         }
                     }
                 }
+                if(currentPixel.hasProperty("spreads")) {
+                    if (currentPixel.getProperty("spreads") == 1){
+                        if (currentPixel.getType() == "fire") {
+                            spread(currentPixel, "flammable");
+                        }
+                    }
+                    else{
+                        currentPixel.changeProperty("spreads", 1);
+                    }
+                }
             }
             reverse = !reverse;
         }
 
         panel.repaint();
     }
+    public void spread(Pixel original, String fuel){
+        int xpos = original.getX();
+        int ypos = original.getY();
+
+        boolean hasFuel = false;
+
+        for (int x = -1; x <= 1; x++){
+            for (int y = -1; y <= 1; y++){
+                if (grid.getPixel(x+xpos, y+ypos).hasProperty("flammable")) {
+                    light(grid.getPixel(x+xpos, y+ypos), original);
+                    if (x == 0 || y == 0) {
+                        hasFuel = true;
+                    }
+                }
+            }
+        }
+
+        if (!hasFuel){
+            grid.setPixel(xpos, ypos, new Air(xpos, ypos));
+        }
+    }
+    public void light(Pixel pixel, Pixel original){
+        int x = pixel.getX();
+        int y = pixel.getY();
+        Pixel check;
+
+        for (int i = -1; i <= 1; i++) {
+            if (i == 0) {
+                check = grid.getPixel(x, y - 1);
+            }
+            else{
+                check = grid.getPixel(x+i, y);
+            }
+
+            if (check.hasProperty("density") && check.getProperty("density") == -1) {
+                Pixel clone = original.duplicate();
+                clone.setY(check.getY());
+                clone.setX(check.getX());
+                clone.changeProperty("spreads", 0);
+                grid.setPixel(check.getX(), check.getY(), clone);
+            }
+        }
+    }
+
+    /*public void spread(Pixel original, String spreadable, boolean staysWithoutFuel){
+        int[] newPositions;
+        boolean stays = staysWithoutFuel;
+
+        int x = original.getX();
+        int y = original.getY();
+
+        int h = grid.getHeight()-1;
+        int w = grid.getWidth()-1;
+
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+
+        if ((x != 0 && y != 0) && grid.getPixel(x-1,y-1).hasProperty(spreadable)) {
+            positions.add(x-1);
+            positions.add(y);
+            positions.add(x);
+            positions.add(y-1);
+        }
+        if ((x != w && y != 0) && grid.getPixel(x+1,y-1).hasProperty(spreadable)) {
+            positions.add(x+1);
+            positions.add(y);
+            positions.add(x);
+            positions.add(y-1);
+        }
+        if ((x != 0 && y != h) && grid.getPixel(x-1,y+1).hasProperty(spreadable)) {
+            positions.add(x-1);
+            positions.add(y);
+            positions.add(x);
+            positions.add(y+1);
+        }
+        if ((x != w && y != h) && grid.getPixel(x+1,y+1).hasProperty(spreadable)) {
+            positions.add(x);
+            positions.add(y+1);
+            positions.add(x+1);
+            positions.add(y);
+        }
+        if ((x != 0) && grid.getPixel(x-1,y).hasProperty(spreadable)) {
+            positions.add(x-1);
+            positions.add(y+1);
+            positions.add(x);
+            positions.add(y);
+        }
+        if ((x != w) && grid.getPixel(x+1,y).hasProperty(spreadable)) {
+            positions.add(x+1);
+            positions.add(y+1);
+            positions.add(x);
+            positions.add(y);
+        }
+        if ((y != h) && grid.getPixel(x,y+1).hasProperty(spreadable)) {
+            positions.add(x-1);
+            positions.add(y+1);
+            positions.add(x+1);
+            positions.add(y+1);
+        }
+        if ((y != 0) && grid.getPixel(x,y-1).hasProperty(spreadable)) {
+            positions.add(x-1);
+            positions.add(y-1);
+            positions.add(x+1);
+            positions.add(y-1);
+            positions.add(x);
+            positions.add(y);
+        }
+
+        for (int i = 0; i < positions.size(); i+= 2){
+            if (!check(grid, positions.get(i), positions.get(i+1))){
+              positions.remove(i+1);
+              positions.remove(i);
+            }
+        }
+
+        for (int i = 0; i < positions.size(); i += 2){
+            Pixel newPixel = original.duplicate();
+            newPixel.setX(positions.get(i));
+            newPixel.setY(positions.get(i+1));
+            grid.setPixel(positions.get(i), positions.get(i+1), newPixel);
+        }
+        5if (!stays){
+            grid.setPixel(original.getX(), original.getY(), new Air(original.getX(), original.getY()));
+        }
+    }
+    public boolean check(Grid grid, int x, int y){
+        return (grid.getPixel(x,y).type.equals("air"));
+    }*/
 }
