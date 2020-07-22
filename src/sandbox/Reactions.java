51bcd2683f94e819e5d4cc53f22aeb005665aa3b
Niko E <piejasper@gmail.com>
2020-07-22 15:04:18 -0700

    small fixes

--- src/sandbox/Reactions.java
+++ src/sandbox/Reactions.java
@@ -7,22 +7,19 @@ import sandbox.pixels.*;
 public class Reactions {
     private HashMap<String, HashMap<String, Pixel>> reactions = new HashMap<String, HashMap<String, Pixel>>();
 
-    public Reactions()
-    {
-        reactions.put("sand", new HashMap<String, Pixel>(){{
+    public Reactions() {
+        reactions.put("sand", new HashMap<>() {{
             put("water", new WetSand(0, 0));
         }});
     }
 
-    public Pixel getReaction(String a, String b)
-    {
+    public Pixel getReaction(String a, String b) {
         return reactions.get(a).get(b);
     }
 
-    public Pixel getReactionOrDefault(String a, String b, Pixel def)
-    {
+    public Pixel getReactionOrDefault(String a, String b, Pixel def) {
         HashMap<String, Pixel> h = reactions.getOrDefault(a, null);
-        if(h == null)
+        if (h == null)
             return def;
         else
             return h.getOrDefault(b, null);
