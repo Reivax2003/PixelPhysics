7c4a74cb8cf1cb252f07c67d23f9b4d8d60a3e18
Xavier Santiago <xavier@santiagoville.net>
2020-07-23 12:42:19 -0700

    part of plant 2

---
+++ src/sandbox/pixels/Plant2.java
@@ -0,0 +1,26 @@
+package sandbox.pixels;
+
+import java.awt.Color;
+import java.util.Random;
+
+public class Plant2 extends Pixel {
+
+    public Plant2(int xpos, int ypos) {
+        super("plant2", xpos, ypos, Color.green.darker());
+        Random r = new Random();
+        this
+                .setProperty("flammable", 1)
+                .setProperty("fuel", 50)
+                .setProperty("density", 10)
+                .setProperty("gravity", 1)
+                .setProperty("support", 1)
+                .setProperty("growing", 1)
+                .setProperty("angle", 20)
+                .setProperty("power", 1)
+                .setProperty("loss", 1)
+                .setProperty("split", 25)
+                .setProperty("turning", r.nextInt(2))
+                .setProperty("direction", 0);
+    }
+
+}
