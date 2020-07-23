f6bd52c1b42aab1cae0618dc6e4fb7719dff4ef8
Xavier Santiago <xavier@santiagoville.net>
2020-07-23 15:12:04 -0700

    Some weird plant was added

--- src/sandbox/pixels/Plant2.java
+++ src/sandbox/pixels/Plant2.java
@@ -6,20 +6,19 @@ import java.util.Random;
 public class Plant2 extends Pixel {
 
     public Plant2(int xpos, int ypos) {
-        super("plant2", xpos, ypos, Color.green.darker());
-        Random r = new Random();
+        super("plant2", xpos, ypos, Color.green.brighter());
         this
                 .setProperty("flammable", 1)
                 .setProperty("fuel", 50)
                 .setProperty("density", 10)
-                .setProperty("gravity", 1)
-                .setProperty("support", 1)
+                //.setProperty("gravity", 0)
+                //.setProperty("support", 1)
                 .setProperty("growing", 1)
                 .setProperty("angle", 20)
-                .setProperty("power", 1)
-                .setProperty("loss", 1)
+                .setProperty("power", 100)
+                .setProperty("loss", 3)
                 .setProperty("split", 25)
-                .setProperty("turning", r.nextInt(2))
+                .setProperty("turning", 0)
                 .setProperty("direction", 0);
     }
 
