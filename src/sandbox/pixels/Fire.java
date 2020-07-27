b0fb10a2f100597e59a0098c3035c56d1d4557dc
Robert Young <ryoungster@gmail.com>
2020-07-27 10:44:07 -0700

    Fire is now dependent on its strength and temp
    Strength is still needed as if the source is very hot, flames grow too tall, but it has been made more lenient

--- src/sandbox/pixels/Fire.java
+++ src/sandbox/pixels/Fire.java
@@ -11,7 +11,7 @@ public class Fire extends Pixel {
                 .setProperty("density", 0)
                 .setProperty("strength", 100) //property for fire to make flickering flames
                 .setProperty("temperature", 150)
-                .setProperty("heating", 3)
+                .setProperty("heating", 2)
                 .setProperty("maxgap", 5);
     }
 
