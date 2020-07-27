a0a645e407c581f19c1508741fb91196afa0ed28
jupiter peng <pengjupiter5@gmail.com>
2020-07-27 10:17:11 -0700

    added gasoline

---
+++ src/sandbox/pixels/Gasoline.java
@@ -0,0 +1,17 @@
+package sandbox.pixels;
+
+import java.awt.*;
+
+public class Gasoline extends Pixel {
+
+    public Gasoline() {
+        super("gasoline", new Color(181, 168, 105));
+        this
+                .setProperty("gravity", 1)
+                .setProperty("density", 50)
+                .setProperty("fluidity", 100)
+                .setProperty("support", 1)
+                .setProperty("flammable", 1)
+                .setProperty("fuel", 10);
+    }
+}
