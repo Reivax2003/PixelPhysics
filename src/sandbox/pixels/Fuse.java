be9cf8c21ff32e640efb50a45359d0b4fb4b60f0
Niko E <piejasper@gmail.com>
2020-07-27 10:32:38 -0700

    add fuse + fuse powder

---
+++ src/sandbox/pixels/Fuse.java
@@ -0,0 +1,13 @@
+package sandbox.pixels;
+
+import java.awt.*;
+
+public class Fuse extends Pixel {
+    public Fuse() {
+        super("fuse", Color.GREEN.darker().darker());
+
+        this
+                .setProperty("flammable", 1)
+                .setProperty("fuel", 50);
+    }
+}
---
+++ src/sandbox/pixels/FusePowder.java
@@ -0,0 +1,17 @@
+package sandbox.pixels;
+
+import java.awt.*;
+
+public class FusePowder extends Pixel {
+    public FusePowder() {
+        super("fuse powder", new Color(119, 198, 109));
+
+        this
+                .setProperty("flammable", 1)
+                .setProperty("fuel", 25)
+                .setProperty("ash", 5)
+                .setProperty("support", 1)
+                .setProperty("gravity", 1)
+                .setProperty("density", 10);
+    }
+}
