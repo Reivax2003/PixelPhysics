a8fa317051ce7c69256bfd81d98981a721deca34
Xavier Santiago <xavier@santiagoville.net>
2020-07-27 14:18:53 -0700

    improved slime

---
+++ src/sandbox/pixels/Grass.java
@@ -0,0 +1,22 @@
+package sandbox.pixels;
+
+import java.awt.*;
+
+public class Grass extends Pixel {
+
+    public Grass() {
+        super("grass", Color.green.darker());
+        this
+                .setProperty("flammable", 1)
+                .setProperty("fuel", 10)
+                .setProperty("charcoal", 2)
+                .setProperty("ash", 1)
+                .setProperty("density", 10)
+                .setProperty("gravity", 1)
+                .setProperty("support", 1)
+                .setProperty("growing", 0)
+                .setProperty("spreads", 1)
+                .setProperty("solubility", 100);
+    }
+
+}
