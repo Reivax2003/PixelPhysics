239d2a055a74b699f81401765c7100aa7798ceff
Robert Young <ryoungster@gmail.com>
2020-07-28 11:42:11 -0700

    Comments in Main menu

--- src/sandbox/MainMenu.java
+++ src/sandbox/MainMenu.java
@@ -14,9 +14,9 @@ public class MainMenu extends JPanel implements ActionListener {
     private final JFileChooser fileChooser = new JFileChooser();
 
     private MainMenu() {
-      fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+      fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir"))); //Sets file viewer to program directory
 
-      JLabel title = new JLabel("Pixel Physics",JLabel.CENTER);
+      JLabel title = new JLabel("Pixel Physics",JLabel.CENTER); //Title
       title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 100));
       title.setHorizontalAlignment(SwingConstants.CENTER);
       title.setVerticalAlignment(SwingConstants.CENTER);
@@ -24,27 +24,28 @@ public class MainMenu extends JPanel implements ActionListener {
 
       add(title);
 
-      JButton creative = new JButton("Creative");
+      JButton creative = new JButton("Creative"); //Freeplay option
       creative.setAlignmentX(Component.CENTER_ALIGNMENT);
       creative.setActionCommand("creative");
       creative.addActionListener(this);
 
       add(creative);
 
-      JButton load = new JButton("Load");
+      JButton load = new JButton("Load"); //Load file option
       load.setAlignmentX(Component.CENTER_ALIGNMENT);
       load.setActionCommand("load");
       load.addActionListener(this);
 
       add(load);
 
-      JButton exit = new JButton("Exit");
+      JButton exit = new JButton("Exit"); //Exit option
       exit.setAlignmentX(Component.CENTER_ALIGNMENT);
       exit.setActionCommand("exit");
       exit.addActionListener(this);
 
       add(exit);
 
+      //Sets vertical box layout
       setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
       displayFrame(this);
@@ -68,25 +69,25 @@ public class MainMenu extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent a) { // Detects interaction with menu
         String action = a.getActionCommand();
-        String[] args = new String[] {};
+        String[] args = new String[] {}; // By default has grid create its new file
 
         if (action.equals("exit")) {
-            System.exit(0);
+            System.exit(0); // Ends the process
         }
         if (action.equals("load")) {
             int option = fileChooser.showOpenDialog(this);
             if(option == JFileChooser.APPROVE_OPTION){
-                args = new String[] {fileChooser.getSelectedFile().getPath()};
+                args = new String[] {fileChooser.getSelectedFile().getPath()}; // Sets arguments to chosen file
             }
             else {
-                return;
+                return; //Stops path if no file is selected
             }
         }
         else if (action.equals("creative")) {
-            args = new String[] {};
+            args = new String[] {}; //Sets default arguments, does nothing
         }
 
-        PixelSandbox.main(args);
+        PixelSandbox.main(args); // Most ways out of the menu will eventually require loading a sandbox
         frame.dispose();
     }
 }
