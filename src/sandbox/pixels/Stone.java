8214dc60233a7921b5670fc49b8e3fc1bf12cef3
Xavier Santiago <xavier@santiagoville.net>
2020-07-30 09:39:55 -0700

    person now gathers food and wood

--- src/sandbox/pixels/Stone.java
+++ src/sandbox/pixels/Stone.java
@@ -10,7 +10,9 @@ public class Stone extends Pixel {
         this.setProperty("solubility", 100)
             .setProperty("temperature", 50)
             .setProperty("heatConduct", 75)
-            .setProperty("cost", 1);
+            .setProperty("cost", 1)
+            .setProperty("building", 1)
+            .setProperty("stone", 1);
     }
 
 }
