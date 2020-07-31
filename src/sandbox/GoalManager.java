4a4f6af6eeeecf81f8c684276b1da98fda9faa23
Robert Young <ryoungster@comcast.net>
2020-07-31 14:23:32 -0700

    Goals Implemented

---
+++ src/sandbox/GoalManager.java
@@ -0,0 +1,31 @@
+package sandbox;
+
+import sandbox.goals.*;
+import sandbox.people.*;
+import java.util.ArrayList;
+
+public class GoalManager {
+
+
+    private final Grid grid;
+    private final PeopleManager peopleManager;
+    private final ArrayList<Goal> goals;
+
+    public GoalManager(Grid grid, PeopleManager peopleManager) {
+        this.grid = grid;
+        this.peopleManager = peopleManager;
+
+        this.goals = grid.getGoals();
+
+
+    }
+
+    public boolean validate() {
+      boolean valid = true;
+      for (Goal each : goals){
+          valid = valid && each.validate(grid, peopleManager);
+      }
+      return valid;
+    }
+
+}
