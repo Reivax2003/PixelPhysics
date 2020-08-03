623d66346ed0b3fe8df7262c304b4ca82d527637
Robert Young <ryoungster@comcast.net>
2020-08-03 11:52:26 -0700

    Generated Placholder level 1

--- src/sandbox/CampaignBuild.java
+++ src/sandbox/CampaignBuild.java
@@ -19,19 +19,19 @@ public class CampaignBuild { //Hardwrite this to create campaign level
         setUpWorld();
         setUpPeople();
         setUpGoals();
-        grid.setName(""); //Set name of level
+        grid.setName("Level 1"); //Set name of level
         grid.saveGrid(new File("campaign/level"+"1"+".lvl"), true); //Saves as a campaign file numbered what is set
     }
 
     private void setUpWorld(){
         //grid.loadGrid("Placeholder"); // Load from pregened world
-        grid.worldGen((long) 12321, "Earth"); // 1st arg seed (long) (Math.floor(Math.random() * 100000) + 1)2nd arg Earth Alien or any text for random type 
+        grid.worldGen((long) 12321, "Earth"); // 1st arg seed (long) (Math.floor(Math.random() * 100000) + 1)2nd arg Earth Alien or any text for random type
     }
     private void setUpPeople(){
         ArrayList<Person> people = grid.getPeople();
         // Test person
-        people.add(new Person(50, 25, "wood"));
-        people.add(new Person(50, 25, "stone"));
+        people.add(new Person(50, 25, new String[]{"gatherer","wood"}));
+        people.add(new Person(50, 25, new String[]{"crafter","tool"}));
     }
     private void setUpGoals(){
         ArrayList<Goal> goals = grid.getGoals();
