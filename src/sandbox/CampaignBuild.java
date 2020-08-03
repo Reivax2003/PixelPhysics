18ffe52e4392d314fcea367253068b27e5705d8d
Niko E <piejasper@gmail.com>
2020-08-03 11:52:11 -0700

    fix build error in CampaignBuild

--- src/sandbox/CampaignBuild.java
+++ src/sandbox/CampaignBuild.java
@@ -1,16 +1,18 @@
 package sandbox;
 
-import java.util.Random;
-import java.util.ArrayList;
-import java.io.File;
+import sandbox.goals.AllHouse;
+import sandbox.goals.Goal;
+import sandbox.goals.MaxHouse;
+import sandbox.people.Person;
+import sandbox.people.WoodAFrame;
+import sandbox.people.WoodShack;
 
-import sandbox.people.*;
-import sandbox.goals.*;
-import sandbox.Grid;
+import java.io.File;
+import java.util.ArrayList;
 
 public class CampaignBuild { //Hardwrite this to create campaign level
 
-    private Grid grid;
+    private final Grid grid;
 
     public CampaignBuild(Grid grid) {
         this.grid = grid;
@@ -20,20 +22,22 @@ public class CampaignBuild { //Hardwrite this to create campaign level
         setUpPeople();
         setUpGoals();
         grid.setName(""); //Set name of level
-        grid.saveGrid(new File("campaign/level"+"1"+".lvl"), true); //Saves as a campaign file numbered what is set
+        grid.saveGrid(new File("campaign/level" + "1" + ".lvl"), true); //Saves as a campaign file numbered what is set
     }
 
-    private void setUpWorld(){
+    private void setUpWorld() {
         //grid.loadGrid("Placeholder"); // Load from pregened world
         grid.worldGen((long) 12321, "Earth"); // 1st arg seed (long) (Math.floor(Math.random() * 100000) + 1)2nd arg Earth Alien or any text for random type 
     }
-    private void setUpPeople(){
+
+    private void setUpPeople() {
         ArrayList<Person> people = grid.getPeople();
         // Test person
-        people.add(new Person(50, 25, "wood"));
-        people.add(new Person(50, 25, "stone"));
+        people.add(new Person(50, 25, new String[]{"wood"}));
+        people.add(new Person(50, 25, new String[]{"stone"}));
     }
-    private void setUpGoals(){
+
+    private void setUpGoals() {
         ArrayList<Goal> goals = grid.getGoals();
         // Test goal
         goals.add(new MaxHouse(new WoodAFrame()));
