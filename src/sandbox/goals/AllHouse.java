272921aa1738becc9731ad85611f85df85785901
Robert Young <ryoungster@gmail.com>
2020-08-03 10:49:34 -0700

    Put repeated code in class

--- src/sandbox/goals/AllHouse.java
+++ src/sandbox/goals/AllHouse.java
@@ -7,30 +7,16 @@ public class AllHouse extends AllGoal { // Whether all houses  of the people are
 
     private static final long serialVersionUID = 8087987;
 
-    private Class goalHouse;
-    private Class[] houseOrder = new Class[]{WoodShack.class, WoodAFrame.class, WoodHouse.class};
-    private int houseIndex;
+    private TestHouse testHouse;
     private String houseName;
 
     public AllHouse(Blueprint goalHouse) {
-        this.goalHouse = goalHouse.getClass();
         this.houseName = goalHouse.getName();
-        for (int i = houseOrder.length-1; i >= 0; i--) { //Gets index of house in house ranking list
-            if (houseOrder[i].equals(this.goalHouse)) {
-              houseIndex = i;
-            }
-        }
+        testHouse = new TestHouse(goalHouse);
     }
 
     protected boolean idividualTest(Person person) {
-        if (person.getHouse() == null) {return false;} // If there is no house, fails
-        Class testHouse = person.getHouse().getClass();
-        for (int i = houseOrder.length-1; i >= houseIndex; i--) { // Go backwards through house values
-            if (houseOrder[i].equals(testHouse)) { // If the house is at a higher or equal index to the goal, it passes
-                return true;
-            }
-        }
-        return false;
+        return testHouse.test(person);
     }
 
     public String getInfo() {
---
+++ src/sandbox/goals/TestHouse.java
@@ -0,0 +1,33 @@
+package sandbox.goals;
+
+import sandbox.*;
+import sandbox.people.*;
+
+public class TestHouse { // Whether the house the person is at a set level or better
+
+    private static final long serialVersionUID = 8087987;
+
+    private Class goalHouse;
+    private Class[] houseOrder = new Class[]{WoodShack.class, WoodAFrame.class, WoodHouse.class};
+    private int houseIndex;
+
+    public TestHouse(Blueprint goalHouse) {
+        this.goalHouse = goalHouse.getClass();
+        for (int i = houseOrder.length-1; i >= 0; i--) { //Gets index of house in house ranking list
+            if (houseOrder[i].equals(this.goalHouse)) {
+              houseIndex = i;
+            }
+        }
+    }
+
+    public boolean test(Person person) {
+        if (person.getHouse() == null) {return false;} // If there is no house, fails
+        Class testHouse = person.getHouse().getClass();
+        for (int i = houseOrder.length-1; i >= houseIndex; i--) { // Go backwards through house values
+            if (houseOrder[i].equals(testHouse)) { // If the house is at a higher or equal index to the goal, it passes
+                return true;
+            }
+        }
+        return false;
+    }
+}
